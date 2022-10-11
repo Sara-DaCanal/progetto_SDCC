10b1a6e23ab2607e09555b4eb39892fe8f1f4667
Sara-DaCanal <s.saradacanal@gmail.com>
2022-10-11 10:39:39 +0200

    script di test

---
+++ codice/test/testing.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: ${0} [OPTIONS]"
+    echo "Options:\n"
+    echo  "\tFLAG   VALUES                    DESCRIPTION"
+    echo  "\t-n     [one | many(5)]           Number of peers to spawn"
+    echo  "\t-a     [auth | token | quorum]   Algorithm to use"
+    echo  "\t-d     [slow | medium | fast]    Level of net congestion"
+}
+
+SIZE=""
+ACTUALSIZE=1
+ALG=""
+DELAY=""
+
+while getopts "hn:a:d:" opt; do
+    case ${opt} in
+        h ) 
+            usage
+            exit 0
+            ;;
+        n )
+            SIZE=${OPTARG}
+            ;;
+        a )
+            ALG=${OPTARG}
+            ;;
+        d )
+            DELAY=${OPTARG}
+            ;;
+        ? )
+            usage
+            exit 1 
+    esac
+done
+shift $((OPTIND -1))
+
+if [ ${SIZE} == "many" ]; then
+    ACTUALSIZE=5
+fi
+
+sh launch.sh -n $ACTUALSIZE -a ${ALG} -v -d ${DELAY} &
+sleep 50
+sh down.sh
+echo "\n\n"
+cd test
+go test
